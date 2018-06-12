c5140192f857f65b2c242157d989110859a3ae05
meatball <xiaoliantuo@126.com>
2018-06-12 16:09:54 +0800

    update

--- js/detail.js
+++ js/detail.js
@@ -2,3 +2,4 @@
 $(".tabs-nav a").click(function(){
 	switchShow(".tabs-nav a",".tabs-content",$(this));
 });
+
--- js/news.js
+++ js/news.js
@@ -1,13 +1,6 @@
-
-$(".tabs a").click(function(){
-	switchShow(".tabs a",".tab-content",$(this));
-});
-$("#setting .tag").click(function(){
-	switchShow("#setting .tag",".tag-content",$(this));
-});
-$(".changeHeader").click(function(){
-	$(".black-layer,.header-uploader").fadeIn();
-});
-$(".header-uploader .close").click(function(){
-	$(".black-layer,.header-uploader").fadeOut();
+$(".switchBar a").click(function(){
+	if(!$(this).hasClass("on")) {
+		$(".switchBar a").removeClass("on");
+		$(this).addClass("on")
+	}
 });
