0fd7361810141578bf1a62015d73ed9a529dc63a
meatball <xiaoliantuo@126.com>
2018-06-15 00:28:59 +0800

    update

--- js/common.js
+++ js/common.js
@@ -14,3 +14,28 @@ $(".switchBar a").click(function(){
 		$(this).addClass("on")
 	}
 });
+
+//闪现泡泡（弹出一秒消失）callPop(状态码,要显示的字符串)
+//状态码说明 0:失败 1:成功 2:
+
+var popTal;
+function callPop(status,txt) {
+	if(popTal) popTal.fadeOut(function(){
+		popTal.remove();
+	});
+	$("body").append("<div class='popWin'><div class='txt'><span>"+txt+"</span></div></div>");
+	popTal = $("body").find(".popWin");
+	switch(status){
+		case 0:
+			$(".popWin").addClass("failure");
+			break;
+		case 1:
+			$(".popWin").addClass("success");
+			break;
+	}
+	$(".popWin").fadeIn(function() {
+		setTimeout(function(){
+			$(".popWin").remove();
+		},700);
+	});
+}
